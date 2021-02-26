6e7ece0b1a2980bfa48562239cef134f781fce65
atulmkamble <kambleatulm@gmail.com>
2021-02-26 15:22:35 +0530

    Adds Day 26 Code

---
+++ Day 26 - Nato Phonetic Codes/main.py
@@ -0,0 +1,20 @@
+# Import required libraries
+import pandas as pd
+
+
+def main():
+    # Read the file
+    df = pd.read_csv('nato_phonetic_alphabet.csv')
+
+    # Create dictionary of letters and corresponding codes
+    nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+
+    # Get the user input and create codes corresponding to letters
+    name = input('Enter your name: ').upper()
+    code_words = [nato_dict[char] for char in name if char in nato_dict.keys()]
+    print('Nato Phonetic Codes:')
+    print(code_words)
+
+
+if __name__ == '__main__':
+    main()
