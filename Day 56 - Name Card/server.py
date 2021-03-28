ddcb8f5c01dfb7a0ee3dcc54b701505069b62ae8
atulmkamble <kambleatulm@gmail.com>
2021-03-28 15:38:17 +0530

    Adds Day 56 Code

---
+++ Day 56 - Name Card/server.py
@@ -0,0 +1,17 @@
+"""
+A Flask server to host the Name Card Website
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
