bd7dd6dc2d58b575d1d85650f2c80bfd1bdc0754
atulmkamble <kambleatulm@gmail.com>
2021-04-23 11:24:18 +0530

    Modifies main file for API Code

--- Day 66 - Cafe with REST API/main.py
+++ Day 66 - Cafe with REST API/main.py
@@ -76,5 +76,24 @@ def get_cafe_at_location():
         return jsonify(error={'Not Found': 'Sorry, we do not have a cafe at that location'})
 
 
+@app.route('/add', methods=['POST'])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={'success': 'Successfully added the new cafe.'})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
