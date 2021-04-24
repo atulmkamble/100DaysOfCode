aff69b701b5b4382092b23e4a2871cfbb5362771
atulmkamble <kambleatulm@gmail.com>
2021-04-24 12:02:49 +0530

    Modifies main file for API Code

--- Day 66 - Cafe with REST API/main.py
+++ Day 66 - Cafe with REST API/main.py
@@ -95,5 +95,17 @@ def post_new_cafe():
     return jsonify(response={'success': 'Successfully added the new cafe.'})
 
 
+@app.route('/update-price/<int:cafe_id>', methods=['PATCH'])
+def patch_new_price(cafe_id):
+    new_price = request.args.get('new_price')
+    cafe = db.session.query(Cafe).get(cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={'success': 'Successfully updated the price.'}), 200
+    else:
+        return jsonify(error={'Not Found': 'Sorry a cafe with that id was not found in the database'}), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
